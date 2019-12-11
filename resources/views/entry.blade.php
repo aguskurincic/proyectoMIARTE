d376d1a5b8ae6929eadc3f8db8a305312a9f7b2a
Your Name <estebangulias@hotmail.com>
2019-12-11 11:20:57 -0300

    carrousel

--- resources/views/entry.blade.php
+++ resources/views/entry.blade.php
@@ -25,15 +25,15 @@
 <!-- Wrapper for slides -->
 <div class="carousel-inner">
 <div class="item active">
-  <img src="Imagenes/producto02.jpg" alt="Los Angeles" style="width:100%;">
+  <img src="/img/abelpintos11.jpg" alt="Los Angeles" style="width:100%;">
 </div>
 
 <div class="item">
-  <img src="Imagenes/producto02.jpg" alt="Chicago" style="width:100%;">
+  <img src="/img/airbaglibertad.jpg" alt="Chicago" style="width:100%;">
 </div>
 
 <div class="item">
-  <img src="Imagenes/producto02.jpg" alt="New york" style="width:100%;">
+  <img src="/img/ceratibocanada.jpg" alt="New york" style="width:100%;">
 </div>
 </div>
 
