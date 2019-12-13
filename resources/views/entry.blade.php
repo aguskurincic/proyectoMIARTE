f199a476bc98c17a1fb12e972046c862c670ebcc
Agustin <agustnk12@gmail.com>
2019-12-13 12:14:20 -0300

    barra busqueda

--- resources/views/entry.blade.php
+++ resources/views/entry.blade.php
@@ -3,8 +3,8 @@
 @section('content')
   <nav class="navbar navbar-light bg-light" id="barrabusqueda">
     <form class="form-inline">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <input class="form-control mr-sm-2" type="search" placeholder="Ejemplo de producto..." aria-label="Search">
+      <button class="btn btn-primary" type="submit">Buscar</button>
     </form>
   </nav>
     </ul>
