cf1b45b47268760c6c4dfb15f0e9f359f113b29e
Agustin <agustnk12@gmail.com>
2019-12-15 20:13:14 -0300

    faltaba algo

--- resources/views/verPerfilUsuario.blade.php
+++ resources/views/verPerfilUsuario.blade.php
@@ -11,7 +11,6 @@
       <h2>Email: {{$usuario->email}}</h2>
 
 
-      <img class="ui medium circular image" src="storage/avatar/{{Auth::user()->avatar}}" width="50%" height="50%">
       <img src="storage/avatar/{{Auth::user()->avatar}}" alt="" id="imgperfil" width="50%" height="50%">
 
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
