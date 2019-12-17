fbf79359e55ad0ba2b600a45eacf71549530e67a
Agustin <agustnk12@gmail.com>
2019-12-17 12:50:37 -0300

    perfil

--- resources/views/verPerfilUsuario.blade.php
+++ resources/views/verPerfilUsuario.blade.php
@@ -1,18 +1,28 @@
 
 @extends('main')
 <title>Perfil Usuario</title>
-<br>
-<br>
 @section('content')
 <br>
 <h1 class="perfilh1" align="center">PERFIL</h1>
+<br>
+<br>
   {{-- <h1>Perfil de usuario</h1>  --}}
     <article class="">
+      {{-- <h1>Usuario: {{$usuario->name}}</h1>
+      <h2>Email: {{$usuario->email}}</h2> --}}
+
+    <div class="fotoperfil">
+
+        <img src="storage/avatar/{{Auth::user()->avatar}}" alt="" id="imgperfil" width="40%" height="40%">
+
+
       <h1>Usuario: {{$usuario->name}}</h1>
       <h2>Email: {{$usuario->email}}</h2>
 
+    </div>
 
-      <img src="storage/avatar/{{Auth::user()->avatar}}" alt="" id="imgperfil" width="50%" height="50%">
+    <br>
+    <br>
 
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </article>
@@ -20,6 +30,7 @@
 
 
       <h1 align='center'>Mis Productos</h1>
+      <br>
 <div class="buttons" id="productoperfilusuario">
         @forelse ($productos as $producto)
           <article class='producto' >
